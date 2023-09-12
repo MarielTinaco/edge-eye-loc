0bfc22ddce7ef05f883365779859b71612204638
Mariel Tinaco <Mariel.Tinaco@analog.com>
2023-09-12 11:39:28 +0800

    add: bounding box drawing

--- src/dataset/image.py
+++ src/dataset/image.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 from dataclasses import dataclass
 from typing import Union, List, Tuple
-from PIL import Image
+from copy import deepcopy
+from PIL import Image, ImageDraw
 
 def clip_x(xy_list : List[Tuple[float, float]], x_sub):
         return list(filter(lambda z : z[0] >= 0, [(x-x_sub, y) for x, y in xy_list]))
@@ -61,6 +62,36 @@ class FaceLandmarkImage:
 
                 ...
 
+        def draw_bbox(self, color='red', eye=False, nose=False, lips=False):
+
+                image_draw = deepcopy(self._image)
+                draw = ImageDraw.Draw(image_draw)
+
+                if eye:
+                        left_eye_x = [x[0] for x in self._landmarks.left_eye]
+                        left_eye_lash_y = [y_lash[1] for y_lash in self._landmarks.left_eye_lash]
+                        left_eye_lid_y = [y_lid[1] for y_lid in self._landmarks.left_eye_lid]
+
+                        left_eye_left_localized = min(left_eye_x)
+                        left_eye_right_localized = max(left_eye_x)
+                        left_eye_top_localized = min(left_eye_lash_y)
+                        left_eye_bottom_localized = max(left_eye_lid_y)
+
+                        draw.rectangle((left_eye_left_localized, left_eye_top_localized, left_eye_right_localized, left_eye_bottom_localized), outline=color)
+
+                        right_eye_x = [z[0] for z in self._landmarks.right_eye]
+                        right_eye_lash_y = [z[1] for z in self._landmarks.right_eye_lash]
+                        right_eye_lid_y = [z[1] for z in self._landmarks.right_eye_lid]
+
+                        right_eye_left_localized = min(right_eye_x)
+                        right_eye_right_localized = max(right_eye_x)
+                        right_eye_top_localized = min(right_eye_lash_y)
+                        right_eye_bottom_localized = max(right_eye_lid_y)
+
+                        draw.rectangle((right_eye_left_localized, right_eye_top_localized, right_eye_right_localized, right_eye_bottom_localized), outline=color)
+
+
+                return image_draw
 
 if __name__ == "__main__":
 
